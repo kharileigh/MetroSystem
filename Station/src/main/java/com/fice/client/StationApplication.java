5dd932d0ca09b549c5b6585e583ee896bda38c57
kharileigh <kharileighmiller@gmail.com>
2022-12-07 13:01:22 +0000

    STATION Project Created

---
+++ Station/src/main/java/com/fice/client/StationApplication.java
@@ -0,0 +1,13 @@
+package com.fice.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationApplication.class, args);
+	}
+
+}
