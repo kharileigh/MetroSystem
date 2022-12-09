d6cada2e2fad30e5b6f9f42af4f0b742b1cd63c6
steph-wright <wright.steph.00@gmail.com>
2022-12-09 07:49:53 +0000

    station resource

---
+++ Station/src/main/java/com/fice/resource/StationResource.java
@@ -0,0 +1,21 @@
+package com.fice.resource;
+
+import com.fice.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StationResource {
+
+    @Autowired
+    StationService stationService;
+
+    @GetMapping(path = "/stations/{sourceStation}/{destinationStation}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public double checkRouteResource(@PathVariable("sourceStation") String sourceStation, @PathVariable("destinationStation") String destinationStation) {
+        return stationService.checkRoute(sourceStation,destinationStation);
+    }
+
+}
