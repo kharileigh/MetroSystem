c15fa04dce97d9dfe771f73f9872b138f7ef244f
lcelik <lidijacelik87@gmail.com>
2022-12-09 11:28:15 +0000

    adding annotations in mains

--- User/src/main/java/com/five/client/UserApplication.java
+++ User/src/main/java/com/five/client/UserApplication.java
@@ -2,8 +2,12 @@ package com.five.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.five")
+@EntityScan(basePackages = "com.five.entity")
+@EnableJpaRepositories(basePackages = "com.five.persistence")
 public class UserApplication {
 
 	public static void main(String[] args) {
